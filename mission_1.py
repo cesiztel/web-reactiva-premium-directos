43ff86f3e1e000e04c1e587f1fb8ba560634e817
Cesar Izquierdo Tello <cesiztel@gmail.com>
2022-10-07 17:21:31 +0200

    feat[mission_1][0]: añadimos el título de la misión y lo que vamos a hacer

--- mission_1.py
+++ mission_1.py
@@ -1,3 +1,14 @@
+"""
+   Misión 1
+
+   ¡Bienvenido agente!. La función get_flagged_cells esta sacada directamente
+   del libro Clean Code. En este libro nos muestran el código de esa
+   función, nos comenta por qué el código tiene problemas y nos muestra
+   la versión final. Para esta misión vamos a refactorizar paso a paso
+   mientras mostramos unos principios de Clean Code y Refactring. 
+   Para hacer el ejemplo más completo he creado un poco de código extra 
+   para darle la forma de una clase de una aplicación real.
+"""
 class Game:
     def __init__(self):
         self.theList = [[0, 0, 2], [0, 1, 2], [0, 2, 4], [0, 3, 5]]
